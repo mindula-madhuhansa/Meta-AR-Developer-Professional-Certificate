754f60754570dc1c28d463cadbc82f57aaa2b846
Mindula Madhuhansa <76749525+mindula-madhuhansa@users.noreply.github.com>
2023-09-12 11:51:40 +0530

    add week 3

---
+++ Course 3 - AR for web using JavaScript/Week 3/Try it/Beating Heart/__settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1854308.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 149464121 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
